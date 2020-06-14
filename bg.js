e1c1dc709f1c25dfb96c8714d609af0515ec2d2c
public_afant <public.afant@gmail.com>
2020-06-15 06:24:47 +0900

    Add BG

--- bg.js
+++ bg.js
@@ -1,5 +1,21 @@
+const COUNT_IMG = 3;
+const body = document.querySelector("body");
+
+const randomNum = () => {
+  return Math.floor(Math.random() * COUNT_IMG);
+};
+
+const paintBgImage = () => {
+  const imgNum = randomNum();
+
+  const image = new Image();
+  image.src = `img/${imgNum + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+};
+
 var init = () => {
-  //   console.log(2);
+  paintBgImage();
 };
 
 init();
